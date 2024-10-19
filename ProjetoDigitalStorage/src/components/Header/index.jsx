27c14f66302a847dac265793d4c21928d48def19
eduardo <bhjkmufdtujh@gmail.com>
2024-10-18 21:27:41 -0300

    header atualizado

---
+++ ProjetoDigitalStorage/src/components/Header/index.jsx
@@ -0,0 +1,35 @@
+import styles from './Header.module.css'
+import img from  '../img/logo-header.svg'
+import cart from '../img/mini-cart.svg'
+
+
+// import { Link } from 'react-router-dom';
+
+
+function Header(){
+    return (
+      <header className={styles.header}>
+           
+            <div className={styles.hea_1}>
+             
+              <img src ={img}  className={styles.header_img}  alt='IMG'></img>  
+              <input type='text' placeholder='Pesquisar produto...'></input>
+                <label className={styles.headerbutton}>Cadastra-se</label>
+                <a href ='/login' className={styles.headerbutton2}>Entrar</a >
+                <div className={styles.div_cart}>
+                <img  src={cart}  className={styles.cart}  alt='carrinho'></img>
+                </div>
+           
+            </div>
+            <div className={styles.hea_2}>
+              
+            <a href ='/home'>Home</a>
+            <a href='/'>Produtos</a>
+            <a href=''>Categorias</a>
+            <a href=''>Meus Pedidos</a>
+            
+            </div>
+      </header>
+    )
+}
+export default Header
