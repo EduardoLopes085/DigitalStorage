821c9d976cd1ac5f6202d2c499d182b2e03cb4e1
eduardo <bhjkmufdtujh@gmail.com>
2024-10-27 22:06:01 -0300

    Fazendo o carrosel

--- ProjetoDigitalStorage/src/components/main/index.jsx
+++ ProjetoDigitalStorage/src/components/main/index.jsx
@@ -1,31 +1,93 @@
-import React,{useState, useEffect} from 'react';
-import  Style from  '../main/main.module.css'
-import Carousel from 'react-bootstrap/Carousel';
+
+import  '../main/main.css';
+import img from '../img/home-slide-3.jpeg'
+import img2 from '../img/home-slide-4.jpeg'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import img1 from '../img/collection-1.png'
-import img2 from '../img/collection-2.png'
-import img3 from '../img/collection-3.png'
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+
+
+
+
 // import img3 from 'DigitalStorage/ProjetoDigitalStorage/src/assets/collection-3.png'
 
 function Main(){
 
   return (
-    <div className={Style.Container} >
-        <Carousel  slide={false}>
-          <Carousel.Item>
-            <img src ={img1}/>
-           
-          </Carousel.Item>
-          <Carousel.Item className={Style.ajs}>
-          <img src ={img3}/>
-           
-          </Carousel.Item>
-          <Carousel.Item>
-          <img src ={img2}/>
-           
-          </Carousel.Item>
-        </Carousel>
+<div className='Container'>
+    <div className="slider">
+    <input type="radio" className="radio_btn" id="radio1" name="radio" />
+    <input type="radio" className="radio_btn" id="radio2" name="radio" />
+    <input type="radio" className="radio_btn" id="radio3" name="radio" />
+    <input type="radio" className="radio_btn" id="radio4" name="radio" />
+  
+    <div className="slides">
+      <div className="slide" id="slide1">
+        <div className='slide_content'>
+          <span>Melhores ofertas personalizadas</span>
+          <span className='textPrincipal'>Queima de stoque Nike 🔥</span>
+          <label className='label_slide'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</label>
+          <button className='btn_slide'>Ver ofertas</button>
+        </div>
+        <div className='slide_img'>
+          <img src="https://i.ibb.co/Jysdmz6/White-Sneakers-PNG-Clipart-2.png" alt="" />
+        </div>
+      </div>
+      <div className="slide" id="slide1">
+     
+        <div className='slide_content'>
+          <span>Melhores ofertas personalizadas</span>
+          <span >🔥 Queima de stoque Nike </span>
+          <label>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt deleniti illo qui.</label>
+          <button className='btn_slide'>Ver ofertas</button>
+        </div>
+        <div id='img1' className='slide_img'>
+            <img src="https://www.rodrigoroehniss.com.br/wp-content/uploads/2021/01/Saucony_Kinvara_12_Site-removebg-preview.png"alt=""/>
+          </div>
+      </div>
+      <div className="slide" id="slide1">
+        <div className='slide_content'>
+          <span>Melhores ofertas personalizadas</span>
+          <span className='textPrincipal'>Queima de stoque Nike 🔥</span>
+          <label className='label_slide'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt deleniti illo qui, cum sunt ipsam praesentium vitae nam dolore tempore laborum, fugit a? Numquam deserunt, omnis nihil accusamus temporibus quos.</label>
+          <button className='btn_slide'>Ver ofertas</button>
+        </div>
+          <div className='slide_img'>
+            <img src="https://i.ibb.co/Jysdmz6/White-Sneakers-PNG-Clipart-2.png" alt="" />
+          </div>
+      </div>
+      <div className="slide" id="slide1">
+        <div className='slide_content'>
+          <span>Melhores ofertas personalizadas</span>
+          <span className='textPrincipal'>Queima de stoque Nike 🔥</span>
+          <label className='label_slide'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt deleniti illo qui, cum sunt ipsam praesentium vitae nam dolore tempore laborum, fugit a? Numquam deserunt, omnis nihil accusamus temporibus quos.</label>
+          <button className='btn_slide'>Ver ofertas</button>
+        </div>
+        <div className='slide_img'>
+          <img src="https://i.ibb.co/Jysdmz6/White-Sneakers-PNG-Clipart-2.png" alt="" />
+        </div>
+      </div>
+      <div className="slide" id="slide1">
+        <div className='slide_content'>
+          <span>Melhores ofertas personalizadas</span>
+          <span className='textPrincipal'>Queima de stoque Nike 🔥</span>
+          <label className='label_slide'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt deleniti illo qui, cum sunt ipsam praesentium vitae nam dolore tempore laborum, fugit a? Numquam deserunt, omnis nihil accusamus temporibus quos.</label>
+          <button className='btn_slide'>Ver ofertas</button>
+        </div>
+        <div className='slide_img'>
+          <img src="https://i.ibb.co/Jysdmz6/White-Sneakers-PNG-Clipart-2.png" alt="" />
+        </div>
+      </div>
     </div>
+  
+    <div className="manual_navigation">
+      <label htmlFor="radio1" className="manual_btn"></label>
+      <label htmlFor="radio2" className="manual_btn"></label>
+      <label htmlFor="radio3" className="manual_btn"></label>
+      <label htmlFor="radio4" className="manual_btn"></label>
+    </div>
+  </div>
+
+  </div>
   );
 }
     
